19bdabf76122cbdd67e3616385bd308d4493b018
Monika <monika.dominic@recordunion.com>
2020-02-13 14:33:09 +0100

    some test function to see if enums work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import {energyInfo} from './enumerations'
 import * as Actions from './actions'
 import './App.css';
 
-const App = props => {
 
+// const checkEnergy = (val) => {
+//   if(val >= 0 && val <= 0.19){
+//     //return 'Not energetic'
+//     setEnergy('Not energetic')
+//   }
+//   else if ( val >= 0.2 && val <= 0.39){
+//     return 'Almost energetic'
+//   }
+//   else if ( val >= 0.4 && val <= 0.59){
+//     return 'Somewhat energetic'
+//   }
+//   else if ( val >= 0.6 && val <= 0.79){
+//     return 'Energetic'
+//   }
+//   else{
+//     return 'Very energetic'
+//   }
+
+// }
+
+
+const App = props => {
+  const [energy, setEnergy] = useState('Not energetic')
   const onChangeSatusClick = () => props.actions.startTest()
+  
+
+  const checkEnergy = (val) => {
+    console.warn(val)
+    if(val >= 0 && val <= 0.19){
+  
+      setEnergy(energyInfo[1])
+    }
+    else if ( val >= 0.2 && val <= 0.39){
+      setEnergy(energyInfo[2])
+    }
+    else if ( val >= 0.4 && val <= 0.59){
+      setEnergy(energyInfo[3])
+    }
+    else if ( val >= 0.6 && val <= 0.79){
+      setEnergy(energyInfo[4])
+    }
+    else{
+      setEnergy(energyInfo[5])
+    }
+  
+  }
 
   return (
     <div className="App">
@@ -14,7 +59,15 @@ const App = props => {
      <br/>
      {props.testStore.status}
      <br/>
+     <div>
      <button onClick={onChangeSatusClick}> Change status </button>
+     </div>
+    <div>
+    <input type="text" placeholder="type a value..." onChange={event => checkEnergy(event.target.value)}/>
+      <br/>
+      <span>{energy}</span>
+    </div>
+       
     </div>
   );
 }
